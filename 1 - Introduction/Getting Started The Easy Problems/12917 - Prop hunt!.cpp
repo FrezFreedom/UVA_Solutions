dfc72c53156824636aa06e18f03d35fa7a03e9c6
frez_freedom <frez1374@gmail.com>
2023-09-11 10:34:46 +0330

    Add 12917 solution

---
+++ 1 - Introduction/Getting Started The Easy Problems/12917 - Prop hunt!.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int p, h, o;
+    while(cin >> p >> h >> o)
+    {
+        if ( o - p >= h )
+        {
+            cout << "Props win!\n";
+        }
+        else
+        {
+            cout << "Hunters win!\n";
+        }
+    }
+    return 0;
+}
